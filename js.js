5625ef6e9e92bd81a0366c941d7de4380eb688de
AzizbekAkramov <akramovazizbek12345@gmail.com>
2025-06-05 07:14:09 +0300

    update

--- js.js
+++ js.js
@@ -4,28 +4,54 @@ const pad = document.getElementById('pad')
 const dap = document.getElementById('dap')
 const apd =document.getElementById('apd')
 const playagain =document.getElementById('playagain')
-				let rando = Math.floor(Math.random()*10);
-        console.log(rando);
+let rando = Math.floor(Math.random() * 10);
+console.log(rando);
 
-  button.onclick = function () {
-if (rando == first.value) {
-	apd.innerHTML = 'You win'
-	apd.style.backgroundColor = 'black'
-	first.remove()
-	button.remove()	
-	pad.remove()
-	dap.remove()
-	playagain.style.opacity='100%'
-}else if (rando <first.value) {
-	dap.innerHTML = `<${first.value}`;
-	playagain.remove()
-}else if (rando >first.value) {
-	pad.innerHTML = `${first.value}>`;
-	playagain.remove()
-}else{
-alert('0 dan 10 gacha raqam kirita olasiz')
-}
-first.value=''
-	}
+let kiritmagan = 0;
+let kiritilgan = 5;
+button.onclick = function () {
+  const userValue = first.value;
+
+  if (
+    userValue.length !== 1 ||
+    userValue < '0' ||
+    userValue > '9'
+  ) {
+    alert("Faqat 0 dan 9 gacha raqam kiriting!");
+    first.value = '';
+    return;
+  }
+
+  kiritmagan++;
+
+  const userNumber = Number(userValue);
+
+  if (kiritmagan > kiritilgan) {
+    apd.innerHTML = "Siz yutqazdingiz!";
+    apd.style.color = "red";
+    button.disabled = true;
+    first.disabled = true;
+    return;
+  }
+
+  if (userNumber === rando) {
+    apd.innerHTML = "You win!";
+    apd.style.backgroundColor = "black";
+    first.remove();
+    button.remove();
+    pad.remove();
+    dap.remove();
+    playagain.style.opacity = "100%";
+  } else if (userNumber < rando) {
+    apd.innerHTML = `Kattaroq son kiriting: ${userNumber}`;
+    apd.style.color = "black"
+    playagain.remove();
+  } else {
+    apd.innerHTML = `Kamroq son kiriting: ${userNumber}`;
+    playagain.remove();
+  }
+
+  first.value = '';
+};
 
 
